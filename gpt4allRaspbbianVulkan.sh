d79ac9034bb58e60584860fea9d99e4f26ed9231
Petri Heinonen <petrix.h@gmail.com>
2024-01-12 22:30:58 +0200

    Added compiled glslc.zip (unzip and copy to right place and make executable if necessary chmod +x ) also added a couple of notes as to where the glslc binary comes from. Unfortunately only x86 versions available to download directly so need to compile for ARM.

--- gpt4allRaspbbianVulkan.sh
+++ gpt4allRaspbbianVulkan.sh
@@ -2,24 +2,28 @@
 
 PATH=$(pwd)
 
-sudo apt-get update 
+sudo apt-get update
 sudo apt-get install -y htop nano git build-essential cmake ninja-build  wget vulkan-tools libvulkan-dev python3-pip
-#sudo apt-get install  mesa-vulkan-drivers 
+#sudo apt-get install  mesa-vulkan-drivers
 
-mkdir cmake 
-cd cmake 
-wget https://github.com/Kitware/CMake/releases/download/v3.28.1/cmake-3.28.1-linux-aarch64.sh 
+mkdir cmake
+cd cmake
+wget https://github.com/Kitware/CMake/releases/download/v3.28.1/cmake-3.28.1-linux-aarch64.sh
 
-chmod +x cmake-3.28.1-linux-aarch64.sh 
+chmod +x cmake-3.28.1-linux-aarch64.sh
 ./cmake-3.28.1-linux-aarch64.sh
 
 cd
 
 ##########
-# 
-# glscl takes about 2h or so to compile... 
+#
+# glscl takes about 2h or so to compile...
 # there is a zip that can let you skip to just copying the binary
 # "sudo cp glslc/glslc /usr/local/bin" after "ninja"
+#
+# https://github.com/google/shaderc
+# https://www.reddit.com/r/linuxmint/comments/s715od/how_to_install_glslc/
+#
 #
 ##########
 
@@ -28,17 +32,17 @@ cd glslc/
 
 #wget https://storage.googleapis.com/shaderc/artifacts/prod/graphics_shader_compiler/shaderc/linux/continuous_clang_release/444/20240103-104401/install.tgz
 
-#tar -xvf install.tgz 
+#tar -xvf install.tgz
 
 #sudo cp install/bin/glslc /usr/local/bin
 
 git clone https://github.com/google/shaderc glslc/
 
-cd glslc 
+cd glslc
 
-./utils/git-sync-deps 
+./utils/git-sync-deps
 
-mkdir build 
+mkdir build
 
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -Bbuild
 
@@ -56,7 +60,7 @@ cd
 #cd kompute/
 #$PATH/cmake/cmake-3.28.1-linux-aarch64/bin/cmake -Bbuild -DKOMPUTE_OPT_DISABLE_VULKAN_VERSION_CHECK=ON
 
- 
+
 cd
 
 
@@ -69,7 +73,7 @@ nano llama.cpp-mainline/ggml-vulkan.cpp
 
 
 
-mkdir build 
+mkdir build
 
 cd build
 
@@ -82,7 +86,7 @@ $PATH/cmake/cmake-3.28.1-linux-aarch64/bin/cmake -S $PATH/gpt4all/gpt4all-backen
 #Comment out memcpy around line 1333
 nano _deps/fmt-src/include/fmt/format.h
 
-$PATH/cmake/cmake-3.28.1-linux-aarch64/bin/cmake --build $PATH/gpt4all/gpt4all-backend/build --parallel 
+$PATH/cmake/cmake-3.28.1-linux-aarch64/bin/cmake --build $PATH/gpt4all/gpt4all-backend/build --parallel
 
 #/home/petri/cmake/cmake-3.28.1-linux-aarch64/bin/cmake --build /home/petri/gpt4all/gpt4all-backend/build --parallel
 
@@ -92,11 +96,11 @@ cd gpt4all/gpt4all-bindings/python
 
 pip3 install -e .
 
-#if pip fails... then (this is NOT how you should do it) 
+#if pip fails... then (this is NOT how you should do it)
 #sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
-# and re run pip3 install.. 
+# and re run pip3 install..
 
-#Test CPU inference first... it will download the model, it's about 2 GB... 
+#Test CPU inference first... it will download the model, it's about 2 GB...
 nano test.py
 
 #from gpt4all import GPT4All
@@ -110,15 +114,11 @@ nano test.py
 #
 #print('Mod load: {}'.format(mod_load - start_time))
 #print('Generate: {}'.format(mod_generate - mod_load))
- 
-python3 test.py 
 
-Then GPU acceleration... 
+python3 test.py
 
-And this is where it fails because it can not find aby GPU devices even though vulkaninfo lists GPU 0 as V3D (the Broadcom Videocore one) and GPU 1 as llvmpipe (which is probably useless here) .. 
+Then GPU acceleration...
+
+And this is where it fails because it can not find aby GPU devices even though vulkaninfo lists GPU 0 as V3D (the Broadcom Videocore one) and GPU 1 as llvmpipe (which is probably useless here) ..
 
 #model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf", device='gpu')
-
-
-
-
